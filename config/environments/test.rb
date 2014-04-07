f3ca2491ed7f9343f8be4b613c683cb7bfa3ed69
Blaine Degannes <blaine@117w.co>
2014-04-06 20:15:29 -0700

    rails_apps_composer: set email accounts

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,8 @@ O17wPrelaunchSignup::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'example.com' }
+
 end
