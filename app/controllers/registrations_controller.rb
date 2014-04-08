afd95fa8dba8e6aad3259d7b101cba779fe10bbc
degannes <blaine@117w.co>
2014-04-07 18:04:35 -0700

    improve design for Request Invitation feature

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,30 @@
 class RegistrationsController < Devise::RegistrationsController
+  
+  # override #create to respond to Ajax with a partial
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      if resource.active_for_authentication?
+        sign_in(resource_name, resource)
+        (render(:partial => 'thankyou', :layout => false) && return)  if request.xhr?
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        expire_session_data_after_sign_in!
+        (render(:partial => 'thankyou', :layout => false) && return)  if request.xhr?
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      render :action => :new, :layout => !request.xhr?
+    end
+  end
+
   protected
 
   def after_inactive_sign_up_path_for(resource)
     # the page prelaunch visitors will see after they request an invitation
+    # unless Ajax is used to return partial
     '/thankyou.html'
   end
 
